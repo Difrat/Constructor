5134e35e60e813380d8581c6bbc1081bf6f13d11
Difrat <bozhenko_m_v@bk.ru>
2024-02-25 22:25:04 +1000

    работа с СУБД

---
+++ Server.py
@@ -0,0 +1,49 @@
+import aiohttp
+from aiohttp import web
+from postgreSQL import DataBaseManager
+import aiohttp_jinja2
+import jinja2
+
+base = DataBaseManager()
+
+app = web.Application()
+aiohttp_jinja2.setup(app,
+                     loader=jinja2.FileSystemLoader('C:\\Users\\Difrat\\Constructor\\Constructor_storage\\templates'))
+
+
+@aiohttp_jinja2.template('index.html')
+async def dynamic_table(request):
+    await base.create_pool()
+    texts = await base.read_from_table('stock')
+    return {'texts': texts}
+
+
+async def handle_form(request):
+    with open('form.html', encoding='utf-8') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+
+async def add_str(request):
+    data = await request.post()
+    name = data.get('name')
+    count = int(data.get('count'))
+    cell = data.get('cell')
+    await base.create_pool()
+    await base.insert_into_table(name, count, cell)
+    return web.HTTPFound('/index')
+
+
+async def handle_submit(request):
+    data = await request.post()
+    name = data.get('name')
+    count = data.get('count')
+    cell = data.get('cell')
+    return web.Response(text=f'Вы ввели имя:{name}, количество:{count}, ячейка:{cell}!')
+
+
+app.router.add_post('/submit', handle_submit)
+app.router.add_post('/add_str', add_str)
+app.router.add_get('/index', dynamic_table)
+app.router.add_get('/', handle_form)
+
+web.run_app(app)
---
+++ postgreSQL.py
@@ -0,0 +1,40 @@
+import asyncio
+import asyncpg
+import jinja2
+import aiohttp_jinja2
+
+
+# CRUD postgreSQL - готово
+# Научится работать с пулом - готово
+# Разабраться с пораметризованными запросами
+class DataBaseManager():
+    def __init__(self):
+        self.pool = None
+
+    async def create_pool(self):
+        self.pool = await asyncpg.create_pool(user='postgres', password='GEl2Few#@;', database='const_db', host='localhost')
+
+    async def create_table(self, name, *args):
+        await self.pool.execute(
+            f'CREATE TABLE IF NOT EXISTS {name}(ID SERIAL PRIMARY KEY, NAME TEXT NOT NULL, COUNT INT NOT NULL, CELL TEXT NOT NULL, DATE DATE NOT NULL);')
+        await self.pool.close()
+
+    async def insert_into_table(self, name_itme, count_item, cell_item):
+        await self.pool.execute('INSERT INTO stock (name, count, cell, date) VALUES ($1, $2, $3, CURRENT_DATE)', name_itme,
+                           count_item, cell_item)
+        await self.pool.close()
+
+    async def read_from_table(self, table_name):
+        result = [dict(r) for r in await self.pool.fetch(f'SELECT id, name, count, cell FROM {table_name}')]
+        await self.pool.close()
+        return result
+
+    async def update_data(self):
+        await self.pool.execute('UPDATE stock SET name = $1 WHERE id = 5;', 'toy')
+        await self.pool.close()
+        print('Значение обновлено!')
+
+    async def delete_data(self):
+        await self.pool.execute('DELETE FROM stock WHERE id = 2')
+        await self.pool.close()
+        print('Строка удалена!')
